ddcecebe314704ea2db87175cc923db95fd97a52
nagraj <nagraj.etc@gmail.com>
2018-06-22 18:39:25 +0530

    Running sample node

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-var express = require('express')
-var app = express()
+// var express = require('express')
+//var app = express()
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+// app.set('port', (process.env.PORT || 5000))
+// //app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+// app.get('/', function(request, response) {
+//   response.send('Hello World!')
+// })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+// app.listen(app.get('port'), function() {
+//   console.log("Node app is running at localhost:" + app.get('port'))
+// })
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+res.writeHead(200);
+res.end('Hi everybody!');
+});
+server.listen(8080);
