f8dd69abd63889e1d708d0b37ac4f40ee42d58b6
nagraj <nagraj.etc@gmail.com>
2018-06-22 19:08:56 +0530

    first batch

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 
-var http = require('http');
+// var http = require("http");
 
-var server = http.createServer(function(req, res) {
-res.writeHead(200);
-res.end('Hi everybody!');
-});
-server.listen(8080);
+// http.createServer(function (request, response) {
+
+//    // Send the HTTP header 
+//    // HTTP Status: 200 : OK
+//    // Content Type: text/plain
+//    response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+//    // Send the response body as "Hello World"
+//    response.end('Hi Nagraj\n');
+// }).listen(3000);
+
+// // Console will print the message
+// console.log('Server running at http://127.0.0.1:'+'3000'+'/');
 
 
-// var express = require('express')
-//var app = express()
+var express = require('express')
+var app = express()
 
-// app.set('port', (process.env.PORT || 5000))
-// //app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 3000))
+//app.use(express.static(__dirname + '/public'))
 
-// app.get('/', function(request, response) {
-//   response.send('Hello World!')
-// })
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
 
-// app.listen(app.get('port'), function() {
-//   console.log("Node app is running at localhost:" + app.get('port'))
-// })
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
