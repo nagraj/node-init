fd327f655899f3e5ba60cd578c5c01381f6c9f24
nagraj <nagraj.etc@gmail.com>
2018-06-22 18:40:53 +0530

    index changed

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+res.writeHead(200);
+res.end('Hi everybody!');
+});
+server.listen(8080);
+
+
 // var express = require('express')
 //var app = express()
 
@@ -11,10 +21,3 @@
 // app.listen(app.get('port'), function() {
 //   console.log("Node app is running at localhost:" + app.get('port'))
 // })
-var http = require('http');
-
-var server = http.createServer(function(req, res) {
-res.writeHead(200);
-res.end('Hi everybody!');
-});
-server.listen(8080);
